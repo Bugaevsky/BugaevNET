a6c8f0ceaf3410bf42033131a4808996d0a9585f
Bugaevsky <55191359+Bugaevsky@users.noreply.github.com>
2020-05-15 16:55:28 +0400

    Add files via upload

---
+++ CTask2/CTask2/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CTask2
+{
+    class Employee : User
+    {
+        private int _experience;
+        private string _position;
+
+        public Employee( string surname, string name, string fathername, DateTime birthday, int age, int experience, string position)
+            : base(surname, name, fathername, birthday, age)
+        {
+            _experience = experience;
+            _position = position;
+        }
+
+        public int Experience { get => _experience;}
+        public string Position { get => _position; }
+    }
+}
---
+++ CTask2/CTask2/FileManager.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+namespace CTask2
+{
+    class FileManager
+    {
+        private const int UserRequiredSymbolsCount = 5;
+        private const int EmployeeRequiredSymbolsCount = 7;
+        public List<User> Reader(string path)
+        {
+            List<User> result = new List<User>(); 
+            using (StreamReader sr = new StreamReader(path))
+            {
+                char[] definitions = { ',', ' ', '.', ':' };
+                string[] users = sr.ReadToEnd().Split('\n');
+                foreach (string user in users)
+                {
+                    string[] temp = user.Split(definitions, StringSplitOptions.RemoveEmptyEntries);
+                    if (temp.Length == UserRequiredSymbolsCount)
+                    {
+
+                        if ((DateTime.TryParse(temp[3], out DateTime DateValue)) && (int.TryParse(temp[4], out int AgeValue)))
+                        {
+                            result.Add(new User(temp[0], temp[1], temp[2], DateValue, AgeValue));
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Data can't be parsed(values is not correct)");
+                        }
+                    }
+                    else if (temp.Length == EmployeeRequiredSymbolsCount)
+                    {
+                        if ((DateTime.TryParse(temp[3], out DateTime DateValue)) && (int.TryParse(temp[4], out int AgeValue)) && (int.TryParse(temp[5], out int ExperienceValue)) && (ExperienceValue >= 0))
+                        {
+                            User user1 = new User(temp[0], temp[1], temp[2], DateValue, AgeValue);
+                            result.Add(new Employee(temp[0], temp[1], temp[2], DateValue, AgeValue, ExperienceValue, temp[6]));
+                        }
+                        else
+                        {
+                            throw new ArgumentException("Data can't be parsed(values is not correct)");
+                        }
+                    }
+                    else
+                    {
+                        throw new ArgumentException("Input string should be 5 or 7 length");
+                    }
+                }
+                return result;
+            }
+        }
+        
+        public void Writer(User user, string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("User:");
+                sw.WriteLine("Surname: " + user.Surname);
+                sw.WriteLine("Name: " + user.Name);
+                sw.WriteLine("Fathername: " + user.Fathername);
+                sw.WriteLine("Birthday: " + user.Birthday.ToShortDateString());
+                sw.WriteLine("Age: " + user.Age);
+            }
+        }
+
+        public void Writer(Employee employee, string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path))
+            {
+                sw.WriteLine("Employee:");
+                sw.WriteLine("Surname: " + employee.Surname);
+                sw.WriteLine("Name: " + employee.Name);
+                sw.WriteLine("Fathername: " + employee.Fathername);
+                sw.WriteLine("Birthday: " + employee.Birthday.ToShortDateString());
+                sw.WriteLine("Age: " + employee.Age);
+                sw.WriteLine("Work Experience: " + employee.Experience);
+                sw.WriteLine("Position: " + employee.Position);
+            }
+        }
+    }
+}
