2d04795c913adf0cfc733fafab2df148f18fa3cc
Bugaevsky <55191359+Bugaevsky@users.noreply.github.com>
2020-06-12 00:16:02 +0400

    Add files via upload

---
+++ CTask3/Entities/Award.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities
+{
+    public class Award
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public List<User> Users { get; set; }
+
+        public Award(int id, string title)
+        {
+            this.ID = id;
+            this.Title = title;
+        }
+
+        public Award(string title)
+        {
+            this.Title = title;
+        }
+
+        public override string ToString()
+        {
+            return $"{ID} {Title}";
+        }
+    }
+}
