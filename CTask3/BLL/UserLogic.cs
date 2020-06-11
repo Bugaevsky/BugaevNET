28e0d3f60078d1984a4d4099152474b7429fdb29
Bugaevsky <55191359+Bugaevsky@users.noreply.github.com>
2020-06-12 00:18:33 +0400

    Add files via upload

---
+++ CTask3/BLL/UserLogic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAL;
+using Entities;
+
+namespace BLL
+{
+    public class UserLogic : IUserLogic
+    {
+        public UserDao _userDao;
+
+        public UserLogic()
+        {
+            _userDao = new UserDao();
+        }
+
+        public string AddUser(User user)
+        {
+            var number = _userDao.AddUser(user);
+            if (number) { return  $"Success"; }
+            else { return $"Error"; }
+        }
+
+        public string DeleteUser(int iDUser)
+        {
+            var number = _userDao.DeleteUser(iDUser);
+            if (number) { return $"Success"; }
+            else { return $"Error"; }
+        }
+
+        public List<string> GetUsers()
+        {
+            var listUsers = _userDao.GetUsers();
+            return listUsers;
+        }
+    }
+}
