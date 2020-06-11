dec35a69f0a2982f8af36a9acea95d764dacf303
Bugaevsky <55191359+Bugaevsky@users.noreply.github.com>
2020-06-12 00:15:31 +0400

    Add files via upload

---
+++ CTask3/DAL/IAwardDao.cs
@@ -0,0 +1,15 @@
+﻿using Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public interface IAwardDao
+    {
+        bool AddAward(Award award);
+        List<Award> GetAwards();
+    }
+}
